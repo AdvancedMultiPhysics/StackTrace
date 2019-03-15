6165c93d67cfa16a45a25c69aa1fc1cf86f36ce1
Mark Berrill <none@none>
2019-03-15 10:14:58 -0400

    Adding make doc target

---
+++ src/doxygen/DoxygenMainpage.h
@@ -0,0 +1,8 @@
+/** \mainpage StackTrace
+ *
+ * This is a set of utilities for catching exceptions and getting the call stack.
+ *
+ *   - \ref StackTrace "StackTrace"
+ *
+ * \author Mark Berrill
+ */
