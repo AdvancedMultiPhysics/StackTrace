7e6d1737cb61c36968b1c2c4c2a65ea3c4c5f731
Grace Lydia Longo - 379751 <glongo@darwin-fe2.lanl.gov>
2024-05-28 16:06:34 -0600

    added timer as an optional variant or the spack package

--- spack-repo/packages/stacktrace/package.py
+++ spack-repo/packages/stacktrace/package.py
@@ -10,16 +10,21 @@ class Stacktrace(CMakePackage):
 
     
     variant("mpi", default=True, description="build with mpi")
+    variant("timer", default=False, description="include timerutility as a dependency")
+
 
     depends_on("cmake@3.26.0:", type="build")
     depends_on("mpi", when="+mpi")
+    depends_on("timerutility+mpi", when="+timer+mpi")
+    depends_on("timerutility~mpi", when="+timer~mpi")
 
     def cmake_args(self):
         args = [
-            "-DCMAKE_CXX_COMPILER=mpic++",
             "-DSTACKTRACE_INSTALL_DIR=" + self.prefix,
             self.define_from_variant("USE_MPI", "mpi"),
-
         ]
+
+        if self.spec.satisfies("^mpi"):
+            args.append("-DCMAKE_CXX_COMPILER=mpicxx")
         return args
 
