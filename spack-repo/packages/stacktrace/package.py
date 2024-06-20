f7871815a1f91c9f4ad131ac4e868e674f4943dd
Richard Berger <rberger@lanl.gov>
2024-06-19 22:12:54 -0600

    spack: update spackage for internal use
    - minor fixes
    - bitbucket repo doesn't have the tags we need for testing.

--- spack-repo/packages/stacktrace/package.py
+++ spack-repo/packages/stacktrace/package.py
@@ -3,11 +3,11 @@ from spack.package import *
 
 class Stacktrace(CMakePackage):
 
-    homepage = "https://bitbucket.org/mberrill/stacktrace/src/master/"
-    git = "https://bitbucket.org/mberrill/stacktrace.git"
+    homepage = "ssh://git@asc-git.lanl.gov:10022/xcap/oss/solvers/stacktrace"
+    git = "ssh://git@asc-git.lanl.gov:10022/xcap/oss/solvers/stacktrace.git"
 
+    version("0.0.92", tag="0.0.92")
     version("0.0.90", tag="0.0.90")
-
     
     variant("mpi", default=True, description="build with mpi")
     variant("timer", default=False, description="include timerutility as a dependency")
@@ -20,11 +20,10 @@ class Stacktrace(CMakePackage):
 
     def cmake_args(self):
         args = [
-            "-DSTACKTRACE_INSTALL_DIR=" + self.prefix,
+            self.define("STACKTRACE_INSTALL_DIR", self.prefix),
             self.define_from_variant("USE_MPI", "mpi"),
         ]
 
         if self.spec.satisfies("^mpi"):
-            args.append("-DCMAKE_CXX_COMPILER=mpicxx")
+            args.append(self.define("CMAKE_CXX_COMPILER", self.spec['mpi'].mpicxx))
         return args
-
